1aa770771c6b57198e4cd8447eb1d1a916741395
LFA-lab <lfhantoine@gmail.com>
2025-03-31 19:44:28 +0200

    Remove solid_cache dependencies

---
+++ db/migrate/20250331172500_drop_solid_cache_entries.rb
@@ -0,0 +1,18 @@
+class DropSolidCacheEntries < ActiveRecord::Migration[8.0]
+  def up
+    drop_table :solid_cache_entries if table_exists?(:solid_cache_entries)
+  end
+
+  def down
+    create_table :solid_cache_entries do |t|
+      t.string :key_hash, null: false
+      t.binary :data, null: false
+      t.integer :byte_size, null: false
+      t.timestamps
+    end
+
+    add_index :solid_cache_entries, :key_hash, unique: true
+    add_index :solid_cache_entries, :byte_size
+    add_index :solid_cache_entries, [:key_hash, :byte_size]
+  end
+end 
